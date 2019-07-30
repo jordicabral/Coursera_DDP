37107123671854304a6823a322e31043159d5965
Jordi Cabral <jordicabral@gmail.com>
2019-07-30 16:01:06 +0200

    Add files via upload

---
+++ Project_DDP_Coursera/ui.R
@@ -0,0 +1,23 @@
+# DDP Coursera - Project TEST
+
+library(shiny)
+library(faraway)
+data("teengamb")
+teengamb$sex <- factor(teengamb$sex)
+levels(teengamb$sex) <- c("Men", "Women")
+
+shinyUI(fluidPage(
+    titlePanel("Teen Gambling Dataset from Faraway package"),
+    sidebarLayout(
+        sidebarPanel(
+            radioButtons("x", "Select X-axis:",
+                         list("sex"='a', "status"='b',
+                              "income"='c', "verbal"='d')),
+            h4("Select a variable to show relationship with gambling expenditure and the regression line")
+            ),   
+        mainPanel(       
+            h3("Relationship between variables"),
+            plotOutput("distPlot")     
+        )   
+    ) 
+))
